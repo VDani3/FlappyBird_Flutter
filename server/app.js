9e3d442526ca18a29f26e3e042e126e5aebd676f
jmartintoro <jmartintoro.cf@iesesteveterradas.cat>
2024-03-11 20:39:24 +0100

    server 0.1

--- server/app.js
+++ server/app.js
@@ -13,13 +13,24 @@ const debug = true
 
     From server to client:
         - Welcome message       { "type": "welcome", "value": "Welcome to the server", "id": 'ABC123' }
+        - If Server is full     { "type": "error", "value": "Server is full"}
         
     From server to everybody (broadcast):
         - All clients data      { "type": "data", "data": [{"id": 'ABC123', "name": 'Abcd', "x": 0, "y": 0 }, {"id": 'XYZ789', "name": 'Wxyz', "x": 0, "y": 0 }] }
+        - Someone connects      { "type": "newClient", id: 'ABC123' }
         - Start game            { "type": "start", "data": ""}
         - Someone loose         { "type": "lost", "data": "{"id": 'ABC123', "name": 'Abcd', "x": 0, "y": 0 }"}
         - All clients lost      { "type": "finnish", "data":"[{ "name": 'Abcd', "puntuation": 0 }, { "name": 'Wxyz', "puntuation": 0 }]"}
 */
+
+///////////////////
+//   VARIABLES   //
+///////////////////
+
+var gameStarted = false;
+var startMessageSended = false;
+var playerLost = [];
+
 
 var ws = new webSockets()
 var gLoop = new gameLoop()
@@ -54,18 +65,25 @@ ws.init(httpServer, port)
 ws.onConnection = (socket, id) => {
   if (debug) console.log("WebSocket client connected: " + id)
 
-  // Saludem personalment al nou client
-  socket.send(JSON.stringify({
-    type: "welcome",
-    value: "Welcome to the server",
-    id: id
-  }))
+  if (!gameStarted) {
+    // Saludem personalment al nou client
+    socket.send(JSON.stringify({
+      type: "welcome",
+      value: "Welcome to the server",
+      id: id
+    }))
 
-  // Enviem el nou client a tothom
-  ws.broadcast(JSON.stringify({
-    type: "newClient",
-    id: id
-  }))
+    // Enviem el nou client a tothom
+    ws.broadcast(JSON.stringify({
+      type: "newClient",
+      id: id
+    }))
+  } else {
+    socket.send(JSON.stringify({
+      type: "error",
+      value: "Server is full"
+    }))
+  }
 }
 
 ws.onMessage = (socket, id, msg) => {
@@ -78,12 +96,18 @@ ws.onMessage = (socket, id, msg) => {
   switch (obj.type) {
     case "init":
       clientData.name = obj.name
-      clientData.color = obj.color
       break;
     case "move":
       clientData.x = obj.x
       clientData.y = obj.y
       break
+    case "loose":
+      clientData.puntuation = obj.puntuation
+      ws.broadcast(JSON.stringify({
+        type: "lost", 
+        data: clientData
+      }))
+      playerLost.push(JSON.stringify({ name: clientData.name, puntuation: clientData.puntuation }))
   }
 }
 
@@ -105,8 +129,25 @@ gLoop.run = (fps) => {
   let clientsData = ws.getClientsData()
 
   // Gestionar aquí la partida, estats i final
-  console.log(clientsData)
+  if (clientsData.length === 4) {
+    gameStarted = true;
+    if (!startMessageSended) {
+      ws.broadcast(JSON.stringify({ type: "start" }))
+    }
+    startMessageSended = true;
+  }
 
-  // Send game status data to everyone
-  ws.broadcast(JSON.stringify({ type: "data", value: clientsData }))
+  if (gameStarted) {
+    console.log(clientsData)  
+    ws.broadcast(JSON.stringify({ type: "data", value: clientsData }))
+  } else {
+
+  }
+
+  if (playerLost.length === 4) {
+    gameLoop = false;
+    ws.broadcast(JSON.stringify({ type: "finnish", data: playerLost }))
+    playerLost = []
+  }
+
 }
